6c64915dad431571680051b01eedb3b47ce9f649
DenizToprakkazar <dtoprakkazar@gmail.com>
2020-08-01 17:07:06 +0300

    ..

---
+++ iAmRichApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iAmRichApp
+//
+//  Created by Deniz Toprakkazar on 1.08.2020.
+//  Copyright © 2020 Deniz Toprakkazar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
